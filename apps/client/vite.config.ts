46264ff773bdedb8a6b30ca13dd52b7f86763589
Alexander Sabouret <asaboure@student.42.fr>
2024-11-23 16:58:10 +0100

    feat: Initialize client application with React, TypeScript, and Tailwind CSS setup

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+    host: true,
+    origin: 'http://0.0.0.O:8080',
+    strictPort: true,
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      '#src': path.resolve(fileURLToPath(new URL('src/', import.meta.url))),
+    },
+  },
+});
