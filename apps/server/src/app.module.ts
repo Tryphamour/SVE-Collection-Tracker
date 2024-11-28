a13292b2da6ac7a75ac276824ceff384ddd2244a
Alexander Sabouret <48692472+kamaboko117@users.noreply.github.com>
2024-11-28 15:29:33 +0100

    feat(server): init back (#2)
    * feat(server): add environment configuration and PostgreSQL service to docker-compose
    
    * feat(server): add nestjs
    
    * feat(server): set global API prefix
    
    * feat(server): link back to db with typeorm + add user CRUD
    
    * refactor(server): rename user-related files for consistency
    
    * refactor(server): rename Users to User for consistency across user module

---
+++ apps/server/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true, // TODO: DO NOT USE IN PRODUCTION
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
