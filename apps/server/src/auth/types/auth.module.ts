d3e2fbc9ed6e12ad6cac7776f8142957d4709d30
Alexander Sabouret <asaboure@student.42.fr>
2025-01-06 18:19:51 +0100

    feat(auth): export AuthService and import UserModule in AuthModule

--- apps/server/src/auth/types/auth.module.ts
+++ apps/server/src/auth/types/auth.module.ts
@@ -3,15 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import googleOauthConfig from 'src/auth/config/google-oauth.config';
 import { User } from 'src/user/entities/user.entity';
+import { UserModule } from 'src/user/user.module';
 import { GoogleStrategy } from '../strategies/google.strategy';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     ConfigModule.forFeature(googleOauthConfig),
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [GoogleStrategy],
+  providers: [GoogleStrategy, AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
