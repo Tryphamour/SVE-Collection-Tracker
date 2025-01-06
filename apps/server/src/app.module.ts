21200971c4da4d1d35d54bcd0dfcb01acfe4ae65
Alexander Sabouret <asaboure@student.42.fr>
2025-01-06 22:16:18 +0100

    fix(auth): update path to authmodule in app module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/types/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
