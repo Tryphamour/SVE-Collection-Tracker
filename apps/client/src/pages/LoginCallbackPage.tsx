a6e155905b94f0e60a5d334e0162b01073f8b98a
Alexander Sabouret <asaboure@student.42.fr>
2025-01-07 15:46:25 +0100

    feat(client): update LoginCallbackPage to redirect and close window after token retrieval

--- apps/client/src/pages/LoginCallbackPage.tsx
+++ apps/client/src/pages/LoginCallbackPage.tsx
@@ -1,14 +1,14 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 export default function LoginCallbackPage() {
   const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
 
   const token = searchParams.get('token');
   
   if (token) {
     window.localStorage.setItem('token', token);
-    navigate('/');
+    window.opener.location.href = '/';
+    window.close();
   }
   return null;
 }
