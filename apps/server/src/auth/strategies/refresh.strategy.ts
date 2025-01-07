7593e3da4d22f65ec0de3d137aaa133dad7c6b04
Alexander Sabouret <asaboure@student.42.fr>
2025-01-07 11:59:17 +0100

    feat(auth): implement refresh token validation in RefreshStrategy

--- apps/server/src/auth/strategies/refresh.strategy.ts
+++ apps/server/src/auth/strategies/refresh.strategy.ts
@@ -1,7 +1,9 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
 import refreshJwtConfig from '../config/refresh-jwt.config';
 import { AuthJwtPayload } from '../types/auth-jwtPayload';
 
@@ -10,14 +12,19 @@ export class RefreshStrategy extends PassportStrategy(Strategy, 'refresh-jwt') {
   constructor(
     @Inject(refreshJwtConfig.KEY)
     private refreshJwtConfiguration: ConfigType<typeof refreshJwtConfig>,
+    private authService: AuthService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: refreshJwtConfiguration.secret,
+      ignoreExpiration: false,
+      passReqToCallback: true,
     });
   }
 
-  validate(payload: AuthJwtPayload) {
-    return { id: payload.sub };
+  validate(req: Request, payload: AuthJwtPayload) {
+    const refreshToken = req.get('authorization').replace('Bearer ', '').trim();
+    const userId = payload.sub;
+    return this.authService.validateRefreshToken(userId, refreshToken);
   }
 }
