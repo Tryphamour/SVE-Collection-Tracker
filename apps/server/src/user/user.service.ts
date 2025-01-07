3e01cd9fcb199fd8066c7f81c8c37f0091d1751c
Alexander Sabouret <asaboure@student.42.fr>
2025-01-07 11:59:37 +0100

    feat(user): implement user creation and retrieval methods with TypeORM

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -1,23 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await this.userRepository.save(createUserDto);
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  findByEmail(email: string) {
+    return this.userRepository.findOneBy({ email });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
+  }
+
+  updateHashedRefreshToken(userId: number, hashedRefreshToken: string) {
+    return this.userRepository.update(userId, { hashedRefreshToken });
   }
 
   remove(id: number) {
