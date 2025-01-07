8254247e2b9113baf4a196ddaa54cd8495a255e1
Alexander Sabouret <asaboure@student.42.fr>
2025-01-07 11:59:06 +0100

    feat(auth): integrate JWT user validation in JwtStrategy

--- apps/server/src/auth/strategies/jwt.strategy.ts
+++ apps/server/src/auth/strategies/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
 import jwtConfig from '../config/jwt-config';
 import { AuthJwtPayload } from '../types/auth-jwtPayload';
 
@@ -10,6 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @Inject(jwtConfig.KEY)
     private jwtConfiguration: ConfigType<typeof jwtConfig>,
+    private authService: AuthService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -18,6 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(payload: AuthJwtPayload) {
-    return { id: payload.sub };
+    const userId = payload.sub;
+    return this.authService.validateJwtUser(userId);
   }
 }
