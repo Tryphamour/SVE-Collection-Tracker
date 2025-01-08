1aae38f3e25a5c98dea6e7cb4d973ec9bce255fe
Alexander Sabouret <asaboure@student.42.fr>
2025-01-08 16:39:35 +0100

    feat(client): add NotFoundPage component and update routing to use it

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import LoginCallbackPage from "./pages/LoginCallbackPage";
-import LoginPage from "./pages/LoginPage";
-
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginCallbackPage from './pages/LoginCallbackPage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<div>About</div>} />
-      <Route path="/login" element={<LoginPage/>} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="loginCallback" element={<LoginCallbackPage />} />
-      <Route path="*" element={<div>Not Found</div>} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
