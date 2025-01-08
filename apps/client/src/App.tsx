7ef5ff871f92943914978c1f2564fb8df04f9a1c
Alexander Sabouret <asaboure@student.42.fr>
2025-01-08 11:39:55 +0100

    feat(theme): add ThemeProvider and useTheme hook for theme management

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import LoginCallbackPage from "./pages/LoginCallbackPage";
-import LoginPage from "./pages/LoginPage";
-
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './components/themeProvider/themeProvider';
+import LoginCallbackPage from './pages/LoginCallbackPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="/about" element={<div>About</div>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="loginCallback" element={<LoginCallbackPage />} />
-      <Route path="*" element={<div>Not Found</div>} />
-    </Routes>
+    <ThemeProvider defaultTheme="system" storageKey='SVECT-UI-THEME'>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="loginCallback" element={<LoginCallbackPage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
