e6ef5bcb75a9dbc03e110d4c42f71a9de12d956e
Alexander Sabouret <48692472+kamaboko117@users.noreply.github.com>
2025-01-10 13:22:51 +0100

    feat: add logout functionality with LogoutPage and server endpoint (#9)

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import LoginCallbackPage from "./pages/LoginCallbackPage";
 import LoginPage from "./pages/LoginPage";
+import LogoutPage from "./pages/LogoutPage";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<div>Home</div>} />
       <Route path="/about" element={<div>About</div>} />
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/logout" element={<LogoutPage />} />
       <Route path="loginCallback" element={<LoginCallbackPage />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Routes>
