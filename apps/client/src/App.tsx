29a2e29d0f4e479daf2a96a2160a31aa085db639
Alexander Sabouret <48692472+kamaboko117@users.noreply.github.com>
2025-01-10 14:44:58 +0100

    feat: home page (#10)
    * chore(client): update favicon to SVECT2.png
    
    * chore(client): add @radix-ui/react-avatar and @radix-ui/react-dropdown-menu dependencies
    
    * feat(client): add layout component with navigation and profile dropdown
    
    * feat(client): add Avatar and DropdownMenu components
    
    * feat(client): implement HomePage component and update routing
    
    * feat(client): add Collapsible component and update dependencies
    
    * feat(client): wrap children in padding for improved layout spacing
    
    * feat(client): add Toggle component with customizable variants and sizes
    
    * feat(client): enhance HomePage with filter, sort, and layout toggle features
    
    * feat(client): add NotFoundPage component and update routing to use it
    
    * feat(client): update navbar links to redirect to home page
    
    * fix(HomePage): adjust layout styles after theme merge

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,19 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './components/themeProvider/themeProvider';
+import HomePage from './pages/HomePage';
 import LoginCallbackPage from './pages/LoginCallbackPage';
 import LoginPage from './pages/LoginPage';
 import LogoutPage from './pages/LogoutPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="SVECT-UI-THEME">
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<div>About</div>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/logout" element={<LogoutPage />} />
         <Route path="loginCallback" element={<LoginCallbackPage />} />
-        <Route path="*" element={<div>Not Found</div>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
   );
---
+++ apps/client/src/pages/HomePage.tsx
@@ -0,0 +1,149 @@
+import Layout from '#src/components/layout';
+import { Button } from '#src/components/ui/button';
+import { Card } from '#src/components/ui/card';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '#src/components/ui/collapsible';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '#src/components/ui/dropdown-menu';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from '#src/components/ui/select';
+import { Toggle } from '#src/components/ui/toggle';
+import { ChevronDown, Filter, Grid2X2, Grid3X3 } from 'lucide-react';
+import { useState } from 'react';
+
+function FilterDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Button variant="outline">
+          <Filter size={24} className="mr-2" />
+          Filter
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>Option 1</DropdownMenuItem>
+        <DropdownMenuItem>Option 2</DropdownMenuItem>
+        <DropdownMenuItem>Option 3</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function SortSelect() {
+  return (
+    <Select>
+      <SelectTrigger className="w-40">Sort</SelectTrigger>
+      <SelectContent>
+        <SelectItem value="option1">Option 1</SelectItem>
+        <SelectItem value="option2">Option 2</SelectItem>
+        <SelectItem value="option3">Option 3</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
+
+function LayoutToggle({
+  layoutMode,
+  setLayoutMode,
+}: {
+  layoutMode: 'gridSM' | 'gridLG';
+  setLayoutMode: (mode: 'gridSM' | 'gridLG') => void;
+}) {
+  return (
+    <div className="flex items-center space-x-2">
+      <Toggle
+        pressed={layoutMode === 'gridSM'}
+        onPressedChange={() => setLayoutMode('gridSM')}
+      >
+        <Grid2X2 size={24} />
+      </Toggle>
+      <Toggle
+        pressed={layoutMode === 'gridLG'}
+        onPressedChange={() => setLayoutMode('gridLG')}
+      >
+        <Grid3X3 size={24} />
+      </Toggle>
+    </div>
+  );
+}
+
+const menuItems = [
+  {
+    title: 'Class',
+    options: ['Option 1', 'Option 2', 'Option 3'],
+  },
+  {
+    title: 'Card type',
+    options: ['Option 1', 'Option 2', 'Option 3'],
+  },
+  {
+    title: 'Trait',
+    options: ['Option 1', 'Option 2', 'Option 3'],
+  },
+];
+
+function CardMenu() {
+  return (
+    <Card className="p-4">
+      {menuItems.map((item) => (
+        <Collapsible key={item.title} defaultOpen>
+          <div className="flex items-center justify-between space-x-4 px-4">
+            <h4 className="text-3xl font-bold">{item.title}</h4>
+            <CollapsibleTrigger>
+              <ChevronDown size={24} />
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <ul className="rounded-md px-4 py-3 text-xl ml-4">
+              {item.options.map((option) => (
+                <li key={option}>{option}</li>
+              ))}
+            </ul>
+          </CollapsibleContent>
+        </Collapsible>
+      ))}
+    </Card>
+  );
+}
+
+function CardList({ layoutMode }: { layoutMode: 'gridSM' | 'gridLG' }) {
+  return (
+    <div
+      className={`p-4 ${layoutMode === 'gridSM' ? 'grid grid-cols-2' : 'grid grid-cols-3'}`}
+    >
+      No Cards
+    </div>
+  );
+}
+
+export default function HomePage() {
+  const [layoutMode, setLayoutMode] = useState<'gridSM' | 'gridLG'>('gridSM');
+  return (
+    <Layout>
+      <div className="flex items-end justify-end h-full w-full space-x-2 mb-2">
+        <FilterDropdown />
+        <SortSelect />
+        <LayoutToggle layoutMode={layoutMode} setLayoutMode={setLayoutMode} />
+      </div>
+      <div className="grid grid-cols-4 gap-4 ">
+        <div className="col-span-1">
+          <CardMenu />
+        </div>
+        <div className="col-span-3">
+          <CardList layoutMode={layoutMode} />
+        </div>
+      </div>
+    </Layout>
+  );
+}
