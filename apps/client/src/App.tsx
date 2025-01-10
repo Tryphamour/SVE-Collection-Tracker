32a70f617e4812b10fe63a80f0f345bd0b538b77
Alexander Sabouret <48692472+kamaboko117@users.noreply.github.com>
2025-01-10 14:36:27 +0100

    feat(theme): add ThemeProvider and useTheme hook for theme management (#8)
    * feat(theme): add ThemeProvider and useTheme hook for theme management
    
    * fix(registerForm): update branding from 'Matcha' to 'SVECT' in registration prompt

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,19 +1,21 @@
-import { Route, Routes } from "react-router-dom";
-import LoginCallbackPage from "./pages/LoginCallbackPage";
-import LoginPage from "./pages/LoginPage";
-import LogoutPage from "./pages/LogoutPage";
-
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './components/themeProvider/themeProvider';
+import LoginCallbackPage from './pages/LoginCallbackPage';
+import LoginPage from './pages/LoginPage';
+import LogoutPage from './pages/LogoutPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="/about" element={<div>About</div>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/logout" element={<LogoutPage />} />
-      <Route path="loginCallback" element={<LoginCallbackPage />} />
-      <Route path="*" element={<div>Not Found</div>} />
-    </Routes>
+    <ThemeProvider defaultTheme="system" storageKey="SVECT-UI-THEME">
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
+        <Route path="loginCallback" element={<LoginCallbackPage />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
